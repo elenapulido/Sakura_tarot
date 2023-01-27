a2314ae4dfe12ad36cbb8cd8043db7e9736b5a53
Jeann11 <jeanneth8464@gmail.com>
2023-01-27 13:31:56 +0100

    imgs

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const url = "https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          SAKURA TARO
-        </a>
-      </header>
+     
+
     </div>
   );
 }
---
+++ src/Component/CallApi.jsx
@@ -0,0 +1,7 @@
+export function CallApi() {
+    const url = "https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/";
+
+    fetch(
+        url
+    )
+}
